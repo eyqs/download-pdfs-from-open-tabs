09f4f68c3b51533f12a855498f9e5e9cdb192cd3
Eugene Y. Q. Shen <eugene@eyqs.ca>
2018-10-02 23:50:38 -0700

    Download PDFs from Canvas too

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 chrome.browserAction.onClicked.addListener(() => {
   chrome.tabs.query({currentWindow: true}, (tabs) => {
+    const regex = /https:\/\/canvas.ubc.ca\/courses\/\d+\/files\/\d+\?module_item_id=\d+/;
     for (const tab of tabs) {
       if (tab.url.toLowerCase().endsWith(".pdf")) {
         chrome.downloads.download({
           url: tab.url,
           filename: `pdfs/${tab.url.split("/").pop()}`,
+        });
+      } else if (regex.test(tab.url.toLowerCase())) {
+        const name = tab.url.split("?")[0];
+        chrome.downloads.download({
+          url: `${name}/download`,
+          filename: `pdfs/${name.split("/").pop()}.pdf`,
         });
       }
     }
